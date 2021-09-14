de4eeb1e8711f719df78eaee2131941b673201a1
unknown <cb5224144@gmail.com>
2021-09-14 16:57:04 -0300

    api-crud escritorio/postman collection atualizada com requisicoes escritorio

---
+++ back-end_AgendaFC/WebApplication1/WebApplication1/Controllers/EscritorioController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Contexts;
+using WebApplication1.Domains;
+using WebApplication1.Interfaces;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EscritorioController : ControllerBase
+    {
+        private IEscritorioRepository _escritorioRepository;
+
+
+        public EscritorioController()
+        {
+            _escritorioRepository = new EscritorioRepository();
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_escritorioRepository.ListarEscritorios());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Escritorio novoEscritorio)
+        {
+            _escritorioRepository.CadastrarEscritorio(novoEscritorio);
+
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Escritorio escritorioAtualizado)
+        {
+            _escritorioRepository.AtualizarEscritorioPorId(id, escritorioAtualizado);
+
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _escritorioRepository.DeletarEscritorio(id);
+
+            return StatusCode(204);
+        }
+    }
+}
---
+++ back-end_AgendaFC/WebApplication1/WebApplication1/Interfaces/IEscritorioRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Domains;
+
+namespace WebApplication1.Interfaces
+{
+    interface IEscritorioRepository
+    {
+        List<Escritorio> ListarEscritorios();
+
+        void CadastrarEscritorio(Escritorio novoEscritorio);
+
+        void DeletarEscritorio(int id);
+
+        void AtualizarEscritorioPorId(int id, Escritorio escritorioAtualizado);
+    }
+}
