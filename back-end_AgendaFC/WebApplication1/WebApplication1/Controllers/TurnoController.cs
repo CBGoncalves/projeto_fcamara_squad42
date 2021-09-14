d5df82e10b8d8166cff50f9016a5fb7efe12eeb2
unknown <cb5224144@gmail.com>
2021-09-14 15:43:15 -0300

    api-crud turnos/postman collection atualizada/organização da pasta back-end

---
+++ back-end_AgendaFC/WebApplication1/WebApplication1/Controllers/TurnoController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Contexts;
+using WebApplication1.Domains;
+using WebApplication1.Interfaces;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TurnoController : ControllerBase
+    {
+        private ITurnoRepository _turnoRepository;
+
+
+        public TurnoController()
+        {
+            _turnoRepository = new TurnoRepository();
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_turnoRepository.ListarTurnos());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Turno novoTurno)
+        {
+            _turnoRepository.CadastrarTurno(novoTurno);
+
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Turno turnoAtualizado)
+        {
+            _turnoRepository.AtualizarTurnoPorId(id, turnoAtualizado);
+
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _turnoRepository.DeletarTurno(id);
+
+            return StatusCode(204);
+        }
+    }
+}
