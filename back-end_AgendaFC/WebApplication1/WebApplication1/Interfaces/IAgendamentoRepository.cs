7b158ef4d5db3d2c1f1adfd320ef2b6556443768
unknown <cb5224144@gmail.com>
2021-09-14 19:23:58 -0300

    api-crud agendamento/postman collection atualizada com requisicoes agendamento

---
+++ back-end_AgendaFC/WebApplication1/WebApplication1/Interfaces/IAgendamentoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Domains;
+
+namespace WebApplication1.Interfaces
+{
+    interface IAgendamentoRepository
+    {
+        List<Agendamento> ListarAgendamentos();
+
+        void CadastrarAgendamento(Agendamento novoAgendamento);
+
+        void DeletarAgendamento(int id);
+
+        Agendamento BuscarAgendamentoPorId(int id);
+
+        void AtualizarAgendamentoPorId(int id, Agendamento agendamentoAtualizado);
+    }
+}
