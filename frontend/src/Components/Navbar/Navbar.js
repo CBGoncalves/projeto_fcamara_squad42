63554d8ee009dcb1e2e4f03f9eee86c0589fb006
estherwl <64702147+estherwl@users.noreply.github.com>
2021-09-16 20:04:16 -0300

    commit

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Logo from '../../imagens/Logo.png';
+import Perfil from '../../imagens/Perfil.png'
+import Sair from '../../imagens/Sair.png'
 
 const Navbar = () => {
   return (    
     <div className="navbar">
       <img src={Logo} className="logoNavbar" alt="Logo"/>
       <span className="agendarMob"><Link to="/Agendar">AGENDAR</Link></span>
-      <span className="meusAgendamentosMob"><Link to="/MeusAgendamentos">MEUS AGENDAMENTOS</Link></span>      
-      <span className="perfilMob">PERFIL</span>
-      <span className="sairMob">SAIR</span>
+      <span className="meusAgendamentosMob"><Link to="/MeusAgendamentos">MEUS AGENDAMENTOS</Link></span>  
+      <img src={Perfil} className="perfilMob" alt="Perfil"/>
+      {/* <span className="perfilMob">PERFIL</span> */}
+      <img src={Sair} className="sairMob" alt="Sair"/>
+      {/* <span className="sairMob">SAIR</span> */}
     </div>
   );
 };
